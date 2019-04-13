ba752dceb2780ac75a991c79dac028405c91aa33
Sahaja <kokkalagadda.sahaja@gmail.com>
2019-04-13 19:31:39 -0400

    Initial commit

---
+++ client-side-demo.js
@@ -0,0 +1,9 @@
+function changeImage() {
+    let picture = document.getElementById("picture");
+    picture.setAttribute("src", "images/jigglypuff.jpg");
+}
+
+function changeText(){
+    let text=document.getElementById("text");
+    text.innerHTML = "new text";
+}
