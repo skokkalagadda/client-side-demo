a248b95cd259fc881ca5b352b6a2029e630ed9c7
Sahaja <kokkalagadda.sahaja@gmail.com>
2019-05-04 19:15:56 -0400

    Added images and gitignore

--- client-side-demo.js
+++ client-side-demo.js
@@ -7,3 +7,13 @@ function changeText(){
     let text=document.getElementById("text");
     text.innerHTML = "new text";
 }
+
+function addImages() {
+  let panda = document.createElement("img");
+  panda.setAttribute("src", "images/smallpanda.png");
+  document.body.append(panda);
+}
+
+addImages();
+
+  
