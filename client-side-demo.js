e712d8bb93f7022f858e91318dc57623aa3aef55
Sahaja <kokkalagadda.sahaja@gmail.com>
2019-05-06 15:32:17 -0400

    interactive code added

--- client-side-demo.js
+++ client-side-demo.js
@@ -4,8 +4,23 @@ function changeImage() {
 }
 
 function changeText(){
+  //we did not give text as id anywhere why is this working
     let text=document.getElementById("text");
-    text.innerHTML = "new text";
+    text.innerHTML = "new text";  
+    let div3=document.getElementById("div3");
+    div3.innerHTML="setNewText";   
+    let div1=document.getElementById("div1");
+    div1.innerHTML="setNewColor";    
+    let div2=document.getElementById("div2");
+    div2.innerHTML="setNewImage";
+
+}
+function extraChallenge(){
+  let div3=document.getElementById("div3");
+    div3.style.color="red";
+
+    var btn=document.getElementById("changeColor");
+  btn.value="changeColour";
 }
 
 function addImages() {
@@ -13,7 +28,15 @@ function addImages() {
   panda.setAttribute("src", "images/smallpanda.png");
   document.body.append(panda);
 }
+function changeColor(){
+  var div1=document.getElementById("div1");
+  div1.className="fuchsiaback";
+  var div2=document.getElementById("div2");
+  div2.className="tealback";
+  var div3=document.getElementById("div3");
+  div2.className="orangeback";
+}
 
-addImages();
+//addImages();
 
   
